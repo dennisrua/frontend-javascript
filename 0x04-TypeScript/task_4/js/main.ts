76ed309d8ec3c5f1973adbde924a0bbf387f19e4
Dennis Rua <ruadennis20@gmail.com>
2025-09-14 23:51:35 +0300

    Made corrections to fit the chexker requirements

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -32,3 +32,8 @@ console.log('React');
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
+
+// const cpp = new CPP();
+// const java = new Java();
+// const react = new React();
+// const cTeacher: Teacher = { firstName: 'John', lastName: 'Doe', experienceTeachingC: 10 };
