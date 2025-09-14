ca86ec8d53199f3a761ea5ff96fad0bd25a99c39
Dennis Rua <ruadennis20@gmail.com>
2025-09-14 22:41:58 +0300

    Made corrections to fit the chexker requirements

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,11 +1,7 @@
 /// <reference path="./Teacher.ts" />
 
 namespace Subjects {
-  export class Subject {
-    teacher!: Teacher;
-
-    setTeacher(teacher: Teacher) {
-      this.teacher = teacher;
-    }
+  export interface Subject {
+    setTeacher(teacher: Teacher): void;
   }
 }
